9f208f6840d914653176ce373602e5afbee570c7
Swapnil Mishra <swapnil.mishra@signavio.com>
2020-08-05 18:38:05 +0200

    integrate ASR client

--- src/App.js
+++ src/App.js
@@ -2,28 +2,79 @@ import React, { useState } from "react";
 import "./App.css";
 import { TextArea } from "./components/textarea/TextArea";
 import { data } from "./mocks/data";
-import { flatten } from "./lib/flatten";
+import { compact, cloneDeep } from "./lib/array";
 import { Transcripts } from "./containers/transcripts/Transcripts";
+import { ASRClient } from "./api/asr/ASRClient";
+import {} from "./lib/array";
+
+const ASRInstance = new ASRClient("wss://vibe-rc.i2x.ai");
 
 const initialWords = ["hello", "how", "You", "span", "div"];
 
 function App() {
   const [matchWords, setMatchWords] = useState(initialWords);
+  const [transcripts, setTranscripts] = useState([data]);
+  const [sessionStarted, setSessionStarted] = useState(false);
+
+  const onSessionStart = (error, results) => {
+    if (!error) {
+      setTranscripts((prevTranscripts) => {
+        const newTranscriptions = cloneDeep(prevTranscripts);
+        const last = newTranscriptions[newTranscriptions.length - 1];
+        last.push(results);
+        return newTranscriptions;
+      });
+    }
+  };
+
+  const startSession = () => {
+    setSessionStarted(true);
+    transcripts.push([]);
+    ASRInstance.start(matchWords, onSessionStart);
+  };
+
+  const stopSession = () => {
+    ASRInstance.stop();
+    setSessionStarted(false);
+  };
+
+  const toggleSession = () => {
+    if (sessionStarted) {
+      stopSession();
+    } else {
+      startSession();
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">Process transcripts</header>
-      <main className="main">
-        <div className="transcripts">
-          <Transcripts transcripts={data} wordList={matchWords} />
-        </div>
-        <div className="match-words">
-          <TextArea
-            textList={matchWords}
-            onChange={(words) => {
-              const wordList = words.split("\n");
-              setMatchWords(flatten(wordList));
-            }}
-          />
+      <h1>{sessionStarted ? "Session started" : "No session"}</h1>
+      <main>
+        <div className="main">
+          <div className="transcripts">
+            {transcripts.map((sessionTranscript, idx) => (
+              <Transcripts
+                key={idx}
+                transcripts={sessionTranscript}
+                wordList={matchWords}
+              />
+            ))}
+          </div>
+          <div className="match-words">
+            <TextArea
+              textList={matchWords}
+              onChange={(words) => {
+                const wordList = words.split("\n");
+                setMatchWords(compact(wordList));
+              }}
+            />
+          </div>
+          <div>
+            <button onClick={toggleSession}>
+              {sessionStarted ? "Stop session" : "Start Session"}
+            </button>
+          </div>
         </div>
       </main>
     </div>
--- src/mocks/data.js
+++ src/mocks/data.js
@@ -16,7 +16,7 @@ export const data = [
       // transcripted words
       utterance: "Message 2: Hello hello, how are you span or a div?",
       // time of the phrase start from the beginning of the session
-      startOffsetMsec: 4730,
+      startOffsetMsec: 4731,
       // time of the phrase end from the beginning of the session
       endOffsetMsec: 5730
     },
@@ -43,6 +43,43 @@ export const data = [
       startOffsetMsec: 10734,
       // time of the phrase end from the beginning of the session
       endOffsetMsec: 11730
+    },
+    // spotted words in this phrase from the `spottingPhrases`
+    spotted: ["hello", "how", "You", "span", "div"]
+  },
+
+  {
+    transcript: {
+      // transcripted words
+      utterance: "Message 4: Hello hello, how are you span or a div?",
+      // time of the phrase start from the beginning of the session
+      startOffsetMsec: 11731,
+      // time of the phrase end from the beginning of the session
+      endOffsetMsec: 11732
+    },
+    // spotted words in this phrase from the `spottingPhrases`
+    spotted: ["hello", "how", "You", "span", "div"]
+  },
+  {
+    transcript: {
+      // transcripted words
+      utterance: "Message 4: Hello hello, how are you span or a div?",
+      // time of the phrase start from the beginning of the session
+      startOffsetMsec: 11733,
+      // time of the phrase end from the beginning of the session
+      endOffsetMsec: 11734
+    },
+    // spotted words in this phrase from the `spottingPhrases`
+    spotted: ["hello", "how", "You", "span", "div"]
+  },
+  {
+    transcript: {
+      // transcripted words
+      utterance: "Message 4: Hello hello, how are you span or a div?",
+      // time of the phrase start from the beginning of the session
+      startOffsetMsec: 11734 + 2001,
+      // time of the phrase end from the beginning of the session
+      endOffsetMsec: 11734 + 2002
     },
     // spotted words in this phrase from the `spottingPhrases`
     spotted: ["hello", "how", "You", "span", "div"]
