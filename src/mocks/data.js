f53a5a82977aa56a5f927fa98688f7d0095efea1
Swapnil Mishra <swapnil.mishra@signavio.com>
2020-08-05 11:48:29 +0200

    add mock data for testing

---
+++ src/mocks/data.js
@@ -0,0 +1,50 @@
+export const data = [
+  {
+    transcript: {
+      // transcripted words
+      utterance: "Message 1: Hello hello, how are you span or a div?",
+      // time of the phrase start from the beginning of the session
+      startOffsetMsec: 420,
+      // time of the phrase end from the beginning of the session
+      endOffsetMsec: 2730
+    },
+    // spotted words in this phrase from the `spottingPhrases`
+    spotted: ["hello", "how", "You", "span", "div"]
+  },
+  {
+    transcript: {
+      // transcripted words
+      utterance: "Message 2: Hello hello, how are you span or a div?",
+      // time of the phrase start from the beginning of the session
+      startOffsetMsec: 4730,
+      // time of the phrase end from the beginning of the session
+      endOffsetMsec: 5730
+    },
+    // spotted words in this phrase from the `spottingPhrases`
+    spotted: ["hello", "how", "You", "span", "div"]
+  },
+  {
+    transcript: {
+      // transcripted words
+      utterance: "Message 3: Hello hello, how are you span or a div?",
+      // time of the phrase start from the beginning of the session
+      startOffsetMsec: 7731,
+      // time of the phrase end from the beginning of the session
+      endOffsetMsec: 8730
+    },
+    // spotted words in this phrase from the `spottingPhrases`
+    spotted: ["hello", "how", "You", "span", "div"]
+  },
+  {
+    transcript: {
+      // transcripted words
+      utterance: "Message 4: Hello hello, how are you span or a div?",
+      // time of the phrase start from the beginning of the session
+      startOffsetMsec: 10734,
+      // time of the phrase end from the beginning of the session
+      endOffsetMsec: 11730
+    },
+    // spotted words in this phrase from the `spottingPhrases`
+    spotted: ["hello", "how", "You", "span", "div"]
+  }
+];
