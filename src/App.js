c96aa5826c82b0fbcba7254fd9f38d2bdffa964b
Swapnil Mishra <swapnil.mishra@signavio.com>
2020-08-10 01:34:59 +0200

    refactored into smaller container components

--- src/App.js
+++ src/App.js
@@ -1,110 +1,25 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useASR } from "./api/asr/useASR";
+import { useSelector } from "react-redux";
 import "./App.css";
-import { TextArea } from "./components/textarea/TextArea";
-import { Transcripts } from "./components/transcripts/Transcripts";
-import { useScrollToBottom } from "./hooks/useScrollToBottom";
-import { compact } from "./lib/array";
-import {
-  markTranscriptsStart,
-  setPhrases,
-  setSessionStatus,
-  setTranscripts
-} from "./store/actions";
-import {
-  SESSION_DISCONNECTED,
-  SESSION_ERRORED,
-  SESSION_STARTED
-} from "./store/consts";
-import { PrimaryButton } from "./components/button/PrimaryButton";
-import { Heading1 } from "./components/typography/Typography";
 import { Flex } from "./components/flex/Flex";
+import { StatusHeading } from "./components/statusHeadling/StatusHeading";
+import { Transcripts } from "./containers/transcripts/Transcripts";
+import { Phrases } from "./containers/phrases/Phrases";
+import { SessionButton } from "./containers/sessionButton/SessionButton";
 
 function App() {
-  const ASRInstance = useASR();
-  const dispatch = useDispatch();
   const sessionStatus = useSelector((state) => state.session);
-  const phrases = useSelector((state) => state.phrases);
-  const transcripts = useSelector((state) => state.transcripts);
-  const [ref] = useScrollToBottom(true);
-
-  const onSessionStart = (error, results) => {
-    if (error) {
-      dispatch(setSessionStatus(SESSION_ERRORED));
-    } else {
-      dispatch(setTranscripts(results));
-    }
-  };
-
-  const startSession = () => {
-    dispatch(setSessionStatus(SESSION_STARTED));
-    dispatch(markTranscriptsStart());
-    ASRInstance.start(phrases, onSessionStart);
-  };
-
-  const stopSession = () => {
-    ASRInstance.stop();
-    dispatch(setSessionStatus(SESSION_DISCONNECTED));
-  };
-
-  const toggleSession = () => {
-    if (sessionStatus === SESSION_STARTED) {
-      stopSession();
-    } else {
-      startSession();
-    }
-  };
-
-  const Phrases = () => {
-    const dispatch = useDispatch();
-    const ASRInstance = useASR();
-    const phrases = useSelector((state) => state.phrases);
-
-    return (
-      <TextArea
-        textList={phrases}
-        onChange={(words) => {
-          const newPhrases = words.split("\n");
-          dispatch(setPhrases(newPhrases));
-          if (ASRInstance.isStarted()) {
-            ASRInstance.updateSpottingConfig(compact(newPhrases));
-          }
-        }}
-      />
-    );
-  };
-
-  const SessionButton = () => {
-    const buttonText =
-      sessionStatus === SESSION_STARTED ? "Stop Session" : "Start Session";
-    return (
-      <PrimaryButton
-        onClick={toggleSession}
-        data-testid="manage-session-button"
-      >
-        {buttonText}
-      </PrimaryButton>
-    );
-  };
 
   return (
     <div className="App">
       <header className="App-header">Process transcripts</header>
       <main>
         <div className="main">
-          <Heading1 data-testid="session-status-heading">
-            <span className="textXL">status:</span>
-            {` session ${sessionStatus}`}
-          </Heading1>
+          <StatusHeading statusText={sessionStatus} />
           <Flex>
             <div>
               <p>Transcript</p>
-              <div className="transcripts" ref={ref}>
-                {transcripts.map((sessionTranscript, idx) => (
-                  <Transcripts key={idx} transcripts={sessionTranscript} />
-                ))}
-              </div>
+              <Transcripts />
             </div>
             <div className="match-words">
               <p>Spotting Phrases</p>
